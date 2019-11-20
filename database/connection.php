29bcd4359185ca2e180ba2e996d9849f4e4a4532
izsi-salmon <izsi.salmon@gmail.com>
2019-11-20 17:32:49 +1300

    completed mobile index

--- database/connection.php
+++ database/connection.php
@@ -1,8 +1,8 @@
 <?php
-//    $dbc = mysqli_connect('', '', '', '');
-  $dbc = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_TABLE'));
+    $dbc = mysqli_connect('localhost', 'root', '', 'portfolio-izsi');
+//  $dbc = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_TABLE'));
+//    var_dump(getenv('PROJECT_URL'));
   if($dbc){
-    echo('Connection successful');
     $dbc->set_charset('utf8');
   } else{
     die('Connect to database failed, please check your environment files and make sure username, password and table names are coorect.');
--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php require 'header.php'; ?>
 
 <?php
-  $sql = 'SELECT * FROM `projects`';
-  $result = mysqli_query($dbc, $sql);
-  if($result){
-    $projects = mysqli_fetch_all($result, MYSQLI_ASSOC);
-  } else{
-    die('Error fetching, no results.');
-  }
-  $table = $_GET['table'];
+    $sql = 'SELECT * FROM `projects`';
+    $result = mysqli_query($dbc, $sql);
+    if($result){
+        $projects = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else{
+        die('Error fetching, no results.');
+    }
+//    $table = $_GET['table'];
+    
+    
 ?>
 
 <div class="main-content">
@@ -18,7 +20,24 @@
     <?php if($projects): ?>
       <div class="projects">
             <?php foreach ($projects as $project): ?>
-                <p>Project</p>
+                <div class="project-block">
+                    <div class="project-img-container">
+                        <img src="images/<?=$project['image_directory']?>/<?=$project['featured_image']?>" alt="project displayed on mock up">
+                    </div>
+                    <div class="project-text">
+                        <a href="#" class="project-title"><h2><?= $project['title']; ?></h2></a>
+                        <a href="#" class="project-title invisible"><h2>invisible</h2></a>
+                        <div class="project-text-details">
+                            <div class="date-container"><div class="date-hr"></div><span class="date"><?=$project['year']?></span></div>
+                            <span class="project-type"><?=$project['project_type']?></span>
+                            <p><?=$project['summary']?></p>
+                            <a href="#" class="view-more-link view-project-container"><i class="fas fa-ellipsis-h project-icon"></i><span>view project</span></a>
+                            <?php // if statement ?>
+                                <a href="#" class="view-more-link view-site-container"><i class="fas fa-globe globe"></i><span>view site</span></a>
+                            <?php //endif; ?>
+                        </div>
+                    </div>
+                </div>
             <?php endforeach; ?>
       </div>
           <?php else: ?>
