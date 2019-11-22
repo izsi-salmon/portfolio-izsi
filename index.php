0cf287e94a898bda4b946be23b45f821cdb1fb7e
izsi-salmon <izsi.salmon@gmail.com>
2019-11-22 18:13:05 +1300

    completed styling for desktop

--- index.php
+++ index.php
@@ -16,24 +16,23 @@
 <div class="main-content">
     <h1>Web/UX <strong>designer</strong> and <strong>developer</strong> in Pōneke (Wellington) Aotearoa.</h1>
     
-    
     <?php if($projects): ?>
       <div class="projects">
             <?php foreach ($projects as $project): ?>
                 <div class="project-block">
                     <div class="project-img-container">
-                        <img src="images/<?=$project['image_directory']?>/<?=$project['featured_image']?>" alt="project displayed on mock up">
+                        <a href="#"><img src="images/<?=$project['image_directory']?>/<?=$project['featured_image']?>" alt="project displayed on mock up"></a>
                     </div>
                     <div class="project-text">
-                        <a href="#" class="project-title"><h2><?= $project['title']; ?></h2></a>
-                        <a href="#" class="project-title invisible"><h2>invisible</h2></a>
-                        <div class="project-text-details">
+                        <div><a href="#" class="project-title" id="projectTitle"><h2><?= $project['title']; ?></h2></a></div>
+<!--                        <a href="#" class="project-title invisible"><h2><?= $project['title']; ?></h2></a>-->
+                        <div class="project-text-details project-text-details-hidden">
                             <div class="date-container"><div class="date-hr"></div><span class="date"><?=$project['year']?></span></div>
                             <span class="project-type"><?=$project['project_type']?></span>
                             <p><?=$project['summary']?></p>
-                            <a href="#" class="view-more-link view-project-container"><i class="fas fa-ellipsis-h project-icon"></i><span>view project</span></a>
+                            <div><a href="#" class="view-more-link view-project-container"><i class="fas fa-ellipsis-h project-icon"></i><span>view project</span></a></div>
                             <?php // if statement ?>
-                                <a href="#" class="view-more-link view-site-container"><i class="fas fa-globe globe"></i><span>view site</span></a>
+                                <div><a href="#" target="_blank" class="view-more-link view-site-container"><i class="fas fa-globe globe"></i><span>view site</span></a></div>
                             <?php //endif; ?>
                         </div>
                     </div>
---
+++ js/script.js
@@ -0,0 +1,7 @@
+var projectImg = document.getElementById('projectImg');
+var projectText = document.getElementById('projectText');
+var projectDetails = document.getElementById('projectDetails');
+
+function showDetails() {
+    projectDetails.style.
+}
