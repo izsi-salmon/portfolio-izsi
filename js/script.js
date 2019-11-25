3186c757f41e489a6f8d542a22d9b3423c66d369
izsi-salmon <izsi.salmon@gmail.com>
2019-11-25 14:10:21 +1300

    added showing and hiding functions for index

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
-var projectImg = document.getElementById('projectImg');
-var projectText = document.getElementById('projectText');
-var projectDetails = document.getElementById('projectDetails');
+var projectBlock = document.getElementsByClassName('project-block');
+var projectImg = document.getElementsByClassName('project-img-container');
+var projectTitle = document.getElementsByClassName('project-title');
+var projectText = document.getElementsByClassName('project-text');
+var projectDetails = document.getElementsByClassName('project-text-details');
 
-function showDetails() {
-    projectDetails.style.
+function showDetails(x) {
+    projectBlock[x].classList.add('project-block-on');
+}
+
+function loopEventListenerFunction() {
+    var i;
+    for (i = 0; i < projectBlock.length; i++) {
+            setEventListeners(i);
+    }
+}
+loopEventListenerFunction();
+
+function setEventListeners(x){
+    projectImg[x].addEventListener('mouseover', function(){showDetails(x);}, false);
+    projectTitle[x].addEventListener('mouseover', function(){showDetails(x);}, false);
+    
+    document.addEventListener('mouseover', function(event) {
+        var imgTarget = projectImg[x].contains(event.target);
+        var textTarget = projectText[x].contains(event.target);
+
+        if (!imgTarget && !textTarget) {
+            projectBlock[x].classList.remove('project-block-on');
+        }
+    });
 }
