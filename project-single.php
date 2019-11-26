74077c8e19cfb1cd56088fbf471afeec3cf0f76b
izsi-salmon <izsi.salmon@gmail.com>
2019-11-26 16:23:08 +1300

    completed desktop project-single

--- project-single.php
+++ project-single.php
@@ -25,21 +25,23 @@
         <?php endforeach; ?>
     </div>
     <div class="text">
-        <h1><?= $project['title'] ?></h1>
-        <div class="date-container">
-            <div class="date"><?= $project['year'] ?></div><div class="date-line"></div>
-        </div>
-        <div class="project-type"><?= $project['project_type'] ?></div>
-        <div class="description">
-            <?= $project['description'] ?>
-        </div>
-        <?php if($project['website_link'] || $project['documentation_link'] || $project['code_link']): ?>
-            <div class="links">
-                <?php if($project['website_link']): ?> <a href="<?= $project['website_link'] ?>" class="icon-link"><i class="fas fa-globe globe"></i><span>visit the site</span></a><?php endif; ?>
-                <?php if($project['documentation_link']): ?> <a href="<?= $project['documentation_link'] ?>" class="icon-link"><i class="far fa-sticky-note"></i><span>view the process</span></a><?php endif; ?>
-                <?php if($project['code_link']): ?> <a href="<?= $project['code_link'] ?>" class="icon-link"><i class="fab fa-github"></i><span>read the code</span></a><?php endif; ?>
+        <div class="text-wrapper">
+            <h1><?= $project['title'] ?></h1>
+            <div class="date-container">
+                <div class="date"><?= $project['year'] ?></div><div class="date-line"></div>
             </div>
-        <?php endif; ?>
+            <div class="project-type"><?= $project['project_type'] ?></div>
+            <div class="description">
+                <?= $project['description'] ?>
+            </div>
+            <?php if($project['website_link'] || $project['documentation_link'] || $project['code_link']): ?>
+                <div class="links">
+                    <?php if($project['website_link']): ?> <a href="<?= $project['website_link'] ?>" class="icon-link"><i class="fas fa-globe globe"></i><span>visit the site</span></a><?php endif; ?>
+                    <?php if($project['documentation_link']): ?> <a href="<?= $project['documentation_link'] ?>" class="icon-link"><i class="far fa-sticky-note"></i><span>view the process</span></a><?php endif; ?>
+                    <?php if($project['code_link']): ?> <a href="<?= $project['code_link'] ?>" class="icon-link"><i class="fab fa-github"></i><span>read the code</span></a><?php endif; ?>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
 
